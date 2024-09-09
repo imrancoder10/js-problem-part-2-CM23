1190971570c8c62a48598b62dcf7c5c56b78bfa9
imrancoder10 <imranhossain1148@gmail.com>
2024-09-09 22:55:23 +0600

    calculator

---
+++ calculator.js
@@ -0,0 +1,38 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1-num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function calculator(a, b, operation){
+    if(operation === 'add'){
+        const result = add(a, b);
+        return result;
+    }
+    else if(operation === 'subtract'){
+        const result = subtract(a, b);
+        return result;
+    }
+    else if(operation === 'multiply'){
+        return multiply(a, b);
+    }
+    else if(operation === 'divide'){
+        return divide(a, b);
+    }
+    else{
+        return "Only 'add', 'subtract', 'multiply', 'divide' operation is allowed." ;
+    }
+}
+
+const result = calculator(15, 7, 'subtract');
+console.log(result);
